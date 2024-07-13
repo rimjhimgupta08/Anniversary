c5502c1bf1259269e28d659e8ef032cf0b6d86fe
Rimjhim Gupta <rimjhimgupta786@gmail.com>
2024-07-13 19:52:15 +0530

    add all file

---
+++ main.js
@@ -0,0 +1,8 @@
+function playSound(audioName){
+      let audio = new Audio(audioName);
+      audio.play();
+      
+}
+// playSound("Mere-Liye-Tum-Kaafi-Ho-Whatsapp-Status---Latest-Whatsapp-Status-2020---Ayushman-Khurana--.mp4");
+playSound("Main-Kaid-Hu-Teri-Lakiro-Me.mp3");
+
